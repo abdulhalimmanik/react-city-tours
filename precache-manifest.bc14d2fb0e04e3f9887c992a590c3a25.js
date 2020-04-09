5ee2081c4b9351b3a16e1cf4e1f6ec89f96afdaa
abdulhalimmanik <abdulhalimmanik@users.noreply.github.com>
2020-04-09 17:57:35 +0600

    Updates

---
+++ precache-manifest.bc14d2fb0e04e3f9887c992a590c3a25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a6659ff5467c71b599841286fb8e7f",
+    "url": "/react-city-tours/static/media/logo.12a6659f.svg"
+  },
+  {
+    "revision": "4d7c8e30f293f7095339",
+    "url": "/react-city-tours/static/js/runtime~main.4d7c8e30.js"
+  },
+  {
+    "revision": "fbfc58c993443de3d096",
+    "url": "/react-city-tours/static/js/main.fbfc58c9.chunk.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/react-city-tours/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "fbfc58c993443de3d096",
+    "url": "/react-city-tours/static/css/main.e91fdd37.chunk.css"
+  },
+  {
+    "revision": "6911e7259b12e0093d7e4d405edcca68",
+    "url": "/react-city-tours/index.html"
+  }
+];
